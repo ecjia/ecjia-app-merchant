c921251f3f9be8ae75815f1bb4be2c96fd5203dd
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-08-09 16:30:58 +0800

    修复方法冲突

--- functions/merchant.func.php
+++ functions/merchant.func.php
@@ -212,7 +212,7 @@ function get_region_name($id)
 /*
 * 管理员操作对象和动作
 */
-function assign_adminlog_content()
+function merchant_assign_adminlog_content()
 {
     ecjia_admin_log::instance()->add_object('merchant', '我的店铺');
 }
--- merchant.php
+++ merchant.php
@@ -72,7 +72,7 @@ class merchant extends ecjia_merchant {
         RC_Script::enqueue_script('migrate', RC_App::apps_url('statics/js/migrate.js', __FILE__) , array() , false, true);
 
         RC_Loader::load_app_func('merchant');
-        assign_adminlog_content();
+        merchant_assign_adminlog_content();
 
         ecjia_merchant_screen::get_current_screen()->add_nav_here(new admin_nav_here('我的店铺', RC_Uri::url('merchant/merchant/init')));
         ecjia_merchant_screen::get_current_screen()->set_parentage('store', 'store/merchant.php');
