b5075d2b39d0735d85e1b5d9b2d6b6540f0fba15
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-11-09 16:42:27 +0800

    商家后台增加小票打印设置

--- apis/merchant_merchant_menu_api.class.php
+++ apis/merchant_merchant_menu_api.class.php
@@ -58,11 +58,12 @@ class merchant_merchant_menu_api extends Component_Event_Api {
         $mymenus = ecjia_merchant::make_admin_menu('14_merchant', __('店铺'), '', 10)->add_icon('fa-home')->add_base('store')->add_purview(array('merchant_manage', 'franchisee_manage', 'bank_manage', 'shopguide_manage'));
         $submenus = array(
             ecjia_merchant::make_admin_menu('01_merchant_setinfo', __('店铺设置'), RC_Uri::url('merchant/merchant/init'), 1)->add_purview('merchant_manage')->add_icon('fa-gears'), //'merchant_info'
-            ecjia_merchant::make_admin_menu('02_merchant_showcase', __('入驻信息'), RC_Uri::url('merchant/mh_franchisee/init'), 2)->add_purview('franchisee_manage')->add_icon('fa-info-circle'), //'enter_info'
+            ecjia_merchant::make_admin_menu('02_merchant_franchisee', __('入驻信息'), RC_Uri::url('merchant/mh_franchisee/init'), 2)->add_purview('franchisee_manage')->add_icon('fa-info-circle'), //'enter_info'
         	ecjia_merchant::make_admin_menu('03_merchant_Receipt', __('收款账号'), RC_Uri::url('merchant/mh_franchisee/receipt'), 3)->add_purview('bank_manage')->add_icon('fa-credit-card'), //'enter_info'
-            ecjia_merchant::make_admin_menu('04_merchant_Receipt', __('店铺上下线'), RC_Uri::url('merchant/merchant/mh_switch'), 4)->add_purview('merchant_switch')->add_icon('fa-power-off'), 
-
-            ecjia_merchant::make_admin_menu('05_merchant_Receipt', __('店铺模版'), RC_Uri::url('merchant/merchant/template'), 5)->add_purview('merchant_manage')->add_icon('fa-navicon'), 
+            ecjia_merchant::make_admin_menu('04_merchant_switch', __('店铺上下线'), RC_Uri::url('merchant/merchant/mh_switch'), 4)->add_purview('merchant_switch')->add_icon('fa-power-off'), 
+            ecjia_merchant::make_admin_menu('05_merchant_template', __('店铺模版'), RC_Uri::url('merchant/merchant/template'), 5)->add_purview('merchant_manage')->add_icon('fa-navicon'), 
+        		
+       		ecjia_merchant::make_admin_menu('06_merchant_printer', __('小票打印设置'), RC_Uri::url('printer/mh_print/init'), 6)->add_purview('printer_manage')->add_icon('fa-gears'),
         );
         
         $mymenus->add_submenu($submenus);
