4b1f9ca75d2a845ff2732ecc6a9bfb1160f7cddf
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-03-09 17:14:14 +0800

    admin/merchant/validate修改

--- modules/admin/merchant/validate_module.class.php
+++ modules/admin/merchant/validate_module.class.php
@@ -52,6 +52,7 @@ defined('IN_ECJIA') or exit('No permission resources.');
  */
 class validate_module extends api_admin implements api_interface {
     public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {
+    	$this->authadminSession();
 		$type		    = $this->requestData('type');
 		$value		    = $this->requestData('value');
 		$validate_type	= $this->requestData('validate_type');
