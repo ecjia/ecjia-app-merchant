30a0e3a31db755a13e0522765d87aba1f35c38e5
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-07-05 09:54:50 +0800

    团购商品列表接口优化

--- modules/merchant/groupbuy/goods/list_module.class.php
+++ modules/merchant/groupbuy/goods/list_module.class.php
@@ -17,75 +17,18 @@ class list_module extends api_front implements api_interface {
 			return new ecjia_error( 'invalid_parameter', RC_Lang::get ('system::system.invalid_parameter' ));
 		}
 		
-		$db_goods_activity = RC_DB::table('goods_activity as ga')
-		->leftJoin('goods as g', RC_DB::raw('ga.goods_id'), '=', RC_DB::raw('g.goods_id'));
+		$options = array(
+				'page'		=> $page,
+				'size'		=> $size,
+				'store_id'	=> array($store_id)
+		);
 		
-		$db_goods_activity
-		->where(RC_DB::raw('ga.act_type'),GAT_GROUP_BUY)
-		->where(RC_DB::raw('ga.start_time'),'<=', RC_Time::gmtime())
-		->where(RC_DB::raw('ga.end_time'),'>=', RC_Time::gmtime())
-		->whereRaw('g.goods_id is not null')
-		->where(RC_DB::raw('g.is_delete'),0)
-		->where(RC_DB::raw('g.is_on_sale'),1)
-		->where(RC_DB::raw('g.is_alone_sale'),1);
-		
-		if (!empty($store_id)) {
-			$db_goods_activity->where(RC_DB::raw('g.store_id'), $store_id);
+		$groupbuy_goods_list = RC_Api::api('groupbuy', 'groupbuy_goods_list', $options);
+		if (is_ecjia_error($groupbuy_goods_list)) {
+			return $groupbuy_goods_list;
 		}
-		
-		if (ecjia::config('review_goods') == 1) {
-			$db_goods_activity->where(RC_DB::raw('g.review_status'), '>', 2);
-		}
-		
-		$count = $db_goods_activity->count(RC_DB::raw('ga.act_id'));
-		$page_row = new ecjia_page($count, $size, 6, '', $page);
-    	
-		$res = $db_goods_activity
-		->selectRaw('ga.*,g.shop_price, g.market_price, g.goods_brief, g.goods_thumb, g.goods_img, g.original_img')
-		->take($size)->skip($page_row->start_id - 1)->orderBy(RC_DB::raw('ga.act_id'),'desc')
-		->get();
-    	$list = array();
-    	if (!empty($res)) {
-    		foreach ($res as $val) {
-    			$ext_info = unserialize($val['ext_info']);
-    			$price_ladder = $ext_info['price_ladder'];
-    			if (!is_array($price_ladder) || empty($price_ladder)) {
-    				$price_ladder = array(array('amount' => 0, 'price' => 0));
-    			} else {
-    				foreach ($price_ladder AS $key => $amount_price) {
-    					$price_ladder[$key]['formated_price'] = price_format($amount_price['price']);
-    				}
-    			}
-    			$cur_price  = $price_ladder[0]['price'];    // 初始化
-    			$list[] = array(
-    					'id'					=> $val['goods_id'],
-    					'name'					=> $val['goods_name'],
-    					'market_price'			=> $val['market_price'],
-    					'formated_market_price'	=> price_format($val['market_price'], false),
-    					'shop_price'			=> $val['shop_price'],
-    					'formated_shop_price'	=> price_format($val['shop_price'], false),
-    					'promote_price'			=> $cur_price,
-    					'formated_promote_price'=> price_format($cur_price, false),
-    					'promote_start_date'	=> RC_Time::local_date('Y/m/d H:i:s', $val['start_time']),
-    					'promote_end_date'		=> RC_Time::local_date('Y/m/d H:i:s', $val['end_time']),
-				    	'img'					=> array(
-						    							'small'	=> empty($val['goods_thumb']) ? '' : RC_Upload::upload_url($val['goods_thumb']),
-														'thumb'	=> empty($val['goods_img']) ? '' : RC_Upload::upload_url($val['goods_img']),
-														'url'	=> empty($val['original_img'])? '' : RC_Upload::upload_url($val['original_img'])						    					),
-    					'goods_activity_id'		=> $val['act_id'],
-    					'activity_type'			=> 'GROUPBUY_GOODS'
-    			);
-    		}
-    	}
-    	
-    	
-    	$pager = array(
-    			"total" => $page_row->total_records,
-    			"count" => $page_row->total_records,
-    			"more"	=> $page_row->total_pages <= $page ? 0 : 1,
-    	);
 
-    	return array('data' => $list, 'pager' => $pager);
+    	return array('data' => $groupbuy_goods_list['list'], 'pager' => $groupbuy_goods_list['page']);
     	
     }
 }
